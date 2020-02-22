444df42701e6ae9f29c40ff18d345522add83fe4
Fdom92 <fer.olmo92@gmail.com>
2020-02-22 18:08:36 +0100

    feat(search): add css transitions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -14,7 +13,6 @@ import { AppComponent } from './app.component';
   entryComponents: [],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule
--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,11 +1,4 @@
 import { Component } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
 import { AvionStackService } from '../shared/services/avionstack.service';
 import { FlightsOptions } from '../models/api-options.model';
 import { Flight } from '../models/flight.model';
@@ -13,49 +6,29 @@ import { Flight } from '../models/flight.model';
 @Component({
   selector: 'app-search',
   templateUrl: 'search.page.html',
-  styleUrls: ['search.page.scss'],
-  animations: [
-    trigger('openClose', [
-      state('closed', style({
-        width: '70%',
-        height: '25%'
-      })),
-      state('opened', style({
-        width: '100%',
-        height: '100%',
-        'border-radius': '0',
-        background: 'rgba(255, 255, 255, 1)'
-      })),
-      transition('closed => opened', [
-        animate('1s')
-      ]),
-      transition('opened => closed', [
-        animate('0.5s')
-      ]),
-    ]),
-  ]
+  styleUrls: ['search.page.scss']
 })
 export class SearchPage {
 
   results: boolean;
 
-  flight: Flight;
-  flights: Array<Flight> = [];
   flightNumber = '';
+  flights: Array<Flight> = [];
+
+  flightSearched = false;
+  flightSelected: Flight;
 
   constructor(private avionStackService: AvionStackService) { }
 
   searchFlight() {
     if (this.flightNumber) {
+      this.flightSearched = true;
       const options: FlightsOptions = {
         flight_iata: this.flightNumber
       };
       this.avionStackService.flights(options).subscribe(flightResponse => {
         this.flights = flightResponse.data;
         this.results = flightResponse.data.length > 0 ? true : false;
-        setTimeout(() => {
-          this.flight = this.flights[0];
-        }, 1100);
       });
     }
   }
@@ -63,7 +36,15 @@ export class SearchPage {
   cancel() {
     this.results = null;
     this.flights = null;
-    this.flight = null;
+    this.flightSearched = false;
   }
 
+
+  openFlight(flight: Flight) {
+    if (this.flightSelected === flight) {
+      this.flightSelected = null;
+    } else {
+      this.flightSelected = flight;
+    }
+  }
 }
